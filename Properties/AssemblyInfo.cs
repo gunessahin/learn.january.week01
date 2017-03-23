f459860e84034780a8efb51b1b696631f9e454b0
Azmi ŞAHİN <azmisahin@outlook.com>
2017-03-23 15:25:21 +0300

    Add a Assembly

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("learn.january.week01")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("learn.january.week01")]
+[assembly: AssemblyCopyright("Telif hakkı ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("538d98d1-7202-4237-a66d-3ddbf3e5b6da")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
